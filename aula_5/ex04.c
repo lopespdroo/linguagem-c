1cf9f1486f24e27563ffb6d3f547cfd9d3e3bd44
Pedro Oliveira <167945158+lopespdroo@users.noreply.github.com>
2025-05-24 00:42:49 -0300

    Update ex04.c
    Ajeitando a condição da linha 6, na qual diz se o ano é bissexto ou não.

--- aula_5/ex04.c
+++ aula_5/ex04.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+include <stdio.h>
 
 int ehBissexto(int ano){
     int valor;
 
-    if ((ano%4 == 0 && ano % 100 == 1) || ano % 400 == 0){
+    if ((ano%4 == 0 && ano % 100 != 0) || ano % 400 == 0){
         valor = 1;
     }
     else {
@@ -25,10 +25,10 @@ int main(){
     printf("%d", bissexto);
 
     if (bissexto == 0){
-        printf("\n%d é um ano bissexto.", ano);
+        printf("\n%d não é um ano bissexto.", ano);
     }
     else{
-        printf("\n%d não é um ano bissexto.", ano);
+        printf("\n%d é um ano bissexto.", ano);
     }
 
     return 0;
